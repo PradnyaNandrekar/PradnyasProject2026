e706e7da0455cea5d0fa9ed8e4c1d2df53e44545
PradnyaNandrekar <167097664+PradnyaNandrekar@users.noreply.github.com>
2026-04-12 15:08:28 +0530

    Update baseClass.java

--- src/test/java/Project_2026_grotechMid/grotechMid_2026/baseClass.java
+++ src/test/java/Project_2026_grotechMid/grotechMid_2026/baseClass.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class baseClass 
 {
@@ -17,9 +18,9 @@ public class baseClass
 	{
 		if(browserName.equalsIgnoreCase("Chrome"))
 		{
-			driver = new ChromeDriver();
 			ChromeOptions options = new ChromeOptions();
 			options.addArguments("--headless");
+			driver = new ChromeDriver(options);
 			driver.manage().window().maximize();
 		}
 		return driver;
